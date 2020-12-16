7b730285b0c7170f0334bde4982761a7efd80cfd
junyeop <yeob1216@gmail.com>
2020-12-17 05:21:21 +0900

    Commit Health & Damage Logic

---
+++ BarrageKingProject/Assets/Logics/Habun/Damage.cs
@@ -0,0 +1,40 @@
+﻿using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Habun
+{
+    public class Damage : MonoBehaviour
+    {
+        [SerializeField]
+        private FloatReference damage;
+        [SerializeField]
+        private LayerMask mask;
+
+        [SerializeField, Space]
+        private ColliderUnityEvent onHit;
+
+        // PUBLIC METHODS: ----------------------------------------------------
+
+        public void Hit(Collider collider)
+        {
+            if ((mask.value & (1 << collider.gameObject.layer)) > 0)
+            {
+                var health = collider.GetComponent<Health>();
+                health.Damage(damage.Value);
+            }
+        }
+
+        // CALLBACK METHODS: ----------------------------------------------------
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider == null)
+            {
+                return;
+            }
+
+            onHit.Invoke(collider);
+        }
+
+    }
+}
---
+++ BarrageKingProject/Assets/Logics/Habun/Health.cs
@@ -0,0 +1,39 @@
+﻿using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Habun
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField]
+        private FloatReference hp;
+        [SerializeField]
+        private FloatReference maxHp;
+
+        [SerializeField, Space]
+        private GameObjectUnityEvent onDeath;
+
+        // PUBLIC METHODS: ----------------------------------------------------
+
+        public void Damage(float damage)
+        {
+            if (hp.Value > 0.0f)
+            {
+                hp.Value = Mathf.Min(0.0f, hp.Value - damage);
+
+                if (hp.Value <= 0.0f)
+                {
+                    onDeath.Invoke(gameObject);
+                }
+            }
+        }
+
+        // MONOBEHAVIOUR METHODS: ----------------------------------------------------
+
+        private void Start()
+        {
+            hp.Value = maxHp.Value;
+        }
+
+    }
+}
